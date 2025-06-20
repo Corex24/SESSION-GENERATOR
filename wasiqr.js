61adc09cd4402c9b330e95f93d85df9217e268c6
Corex_Anthony <desireanthony24@gmail.com>
2025-06-20 03:19:17 +0100

    Update wasiqr.js

--- wasiqr.js
+++ wasiqr.js
@@ -58,26 +58,19 @@ router.get('/', async (req, res) => {
 				   let b64data = Buffer.from(data).toString('base64');
 				   let session = await Qr_Code_By_Wasi_Tech.sendMessage(Qr_Code_By_Wasi_Tech.user.id, { text: '' + b64data });
 	
-				   let WASI_MD_TEXT = `
-*_Session Connected By Wasi Tech_*
-*_Made With 🤍_*
-______________________________________
-╔════◇
-║ *『AMAZING YOU'VE CHOSEN WASI MD』*
-║ _You Have Completed the First Step to Deploy a Whatsapp Bot._
-╚════════════════════════╝
-╔═════◇
-║  『••• 𝗩𝗶𝘀𝗶𝘁 𝗙𝗼𝗿 𝗛𝗲𝗹𝗽 •••』
-║❒ *Ytube:* _youtube.com/@wasitech1
-║❒ *Owner:* _https://wa.me/message/THZ3I25BYZM2E1_
-║❒ *Repo:* _https://github.com/wasixd/WASI-MD_
-║❒ *WaGroup:* _https://chat.whatsapp.com/FF6YuOZTAVB6Lu65cnY5BN_
-║❒ *WaChannel:* _https://whatsapp.com/channel/0029VaDK8ZUDjiOhwFS1cP2j_
-║❒ *Plugins:* _https://github.com/Itxxwasi 
-╚════════════════════════╝
-_____________________________________
-	
-_Don't Forget To Give Star To My Repo_`
+				   let WASI_MD_TEXT = `🤖 𝗔𝗡𝗖𝗢𝗥𝗘 𝗕𝗢𝗧:
+✅ Session successfully generated!
+
+Here’s everything you need to stay in touch with Ancore:
+
+📣 *Telegram Channel:*
+https://t.me/corex2410
+
+💬 *WhatsApp Channel:*
+https://whatsapp.com/channel/0029Vb6nHar2phHORBFhOn3p
+
+> ⚡ Powered by Corex
+`
 	 await Qr_Code_By_Wasi_Tech.sendMessage(Qr_Code_By_Wasi_Tech.user.id,{text:WASI_MD_TEXT},{quoted:session})
 
 
